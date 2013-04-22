53b673c0bce712af0052e78b017eb197b04f56a7
Ryan Clark <ryan.clark.j@gmail.com>
2013-04-22 15:34:24 -0700

    use binaries locally

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-coffee -c src/*.coffee scripts/
-jade src/index.jade --out ./
-lessc src/base.less styles/base.css
+./node_modules/coffee-script/bin/coffee -c src/*.coffee scripts/
+./node_modules/jade/bin/jade src/index.jade --out ./
+./node_modules/less/bin/lessc src/base.less styles/base.css
 coffee -c -o scripts/ src/*.coffee
