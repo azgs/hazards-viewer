e81e4fd0edb021e0bd28a17c5ebca2ccb7d5dc89
Ryan Clark <ryan.clark.j@gmail.com>
2013-04-22 15:40:13 -0700

    adjust install script

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-./node_modules/coffee-script/bin/coffee -c src/*.coffee scripts/
+./node_modules/coffee-script/bin/coffee -c -o scripts/ src/*.coffee
 ./node_modules/jade/bin/jade src/index.jade --out ./
 ./node_modules/less/bin/lessc src/base.less styles/base.css
-coffee -c -o scripts/ src/*.coffee
